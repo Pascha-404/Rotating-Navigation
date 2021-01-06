96fa14584384137cced8658d667932a4e7cfe300
Patrick Pavliuchik <73493957+Pascha-404@users.noreply.github.com>
2021-01-06 23:12:35 +0100

    Files

---
+++ script.js
@@ -0,0 +1,11 @@
+const closeBtn = document.querySelector('#close');
+const openBtn = document.querySelector('#open');
+const container = document.querySelector('.container')
+
+openBtn.addEventListener('click', () =>{
+container.classList.add('show-nav');
+})
+
+closeBtn.addEventListener('click', () =>{
+    container.classList.remove('show-nav');
+    })
